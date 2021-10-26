c83447240c4f9421208be6c96030facfda446e02
Andy <awilliamscomposing@gmail.com>
2021-10-25 18:22:19 -0700

    Installing bootstrap dependencies, starting CSS over

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,8 @@ const Home = () => {
     <>
       <p>Portrait</p>
       {/* <img src="placeholder.png" class="img-fluid" alt="Portrait placeholder" /> */}
-      <section id="hero" class="d-flex align-items-center">
-        <div
-          class="container d-flex flex-column align-items-center"
-          data-aos="zoom-in"
-          data-aos-delay="100"
-        >
+      <section>
+        <div class="container" data-aos="zoom-in" data-aos-delay="100">
           <h1 class="text-light">Andy Williams</h1>
           <h2 class="text-light">
             Full-stack developer based in Portland, Oregon
