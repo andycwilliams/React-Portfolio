13a26d25193cb143d707bf90364eaa6a7d6299a4
Andy <awilliamscomposing@gmail.com>
2021-10-30 22:32:42 -0700

    Organizing layout more

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Col, Container, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 // import bg from "../img/Redwoods.jpg";
 import "../App.css";
 
@@ -10,7 +10,22 @@ const Home = () => {
         <Container fluid="md">
           <Row className="justify-content-md-center">
             <Col md="auto">
-              <div>
+              <Card border="primary" style={{ width: "18rem" }}>
+                <Card.Body>
+                  <Card.Title>Andy Williams</Card.Title>
+                  <img
+                    src="/img/Placeholder.png"
+                    class="img-fluid"
+                    alt="Portrait of developer"
+                    style={{ width: "200px", height: "200px" }}
+                  />
+                  <Card.Text>
+                    Full-stack developer based in Portland, Oregon
+                  </Card.Text>
+                  <Button>About Me</Button>
+                </Card.Body>
+              </Card>
+              {/* <div>
                 <img
                   src="/img/Placeholder.png"
                   class="img-fluid"
@@ -20,7 +35,7 @@ const Home = () => {
                 <h1>Andy Williams</h1>
                 <h3>Full-stack developer based in Portland, Oregon</h3>
                 <Button>About Me</Button>
-              </div>
+              </div> */}
             </Col>
           </Row>
         </Container>
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,35 +1,49 @@
 import React from "react";
-import { Button, Card, Container, Row } from "react-bootstrap";
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  Col,
+  Container,
+  Row,
+} from "react-bootstrap";
 
 const Portfolio = () => {
   // const imgStyle = { width: "200px", height: "200px" };
   return (
     <>
+      <br></br>
       <Container fluid="md">
-        <h2>Portfolio</h2>
-        <p>Select projects.</p>
+        <Row className="justify-content-md-center">
+          <Col md="auto">
+            <h2>Portfolio</h2>
+            <p>Select projects.</p>
+          </Col>
+        </Row>
       </Container>
 
       <Container fluid="md">
-        <Row>
+        <Row className="justify-content-md-center">
           <Card style={{ width: "18rem" }}>
             <Card.Img variant="top" src="/img/Placeholder.png" />
             <Card.Body>
               <Card.Title>Productivity Tracker</Card.Title>
               <Card.Subtitle>Solo Project</Card.Subtitle>
               <Card.Text>App description.</Card.Text>
-              <Button
-                variant="primary"
-                href="https://github.com/andycwilliams/ProductivityTracker-PersonalProject2.5"
-              >
-                GitHub
-              </Button>
-              <Button
-                variant="secondary"
-                href="https://github.com/andycwilliams/ProductivityTracker-PersonalProject2.5"
-              >
-                Deployed
-              </Button>
+              <ButtonGroup aria-label="Basic example">
+                <Button
+                  variant="primary"
+                  href="https://github.com/andycwilliams/ProductivityTracker-PersonalProject2.5"
+                >
+                  GitHub
+                </Button>
+                <Button
+                  variant="secondary"
+                  href="https://github.com/andycwilliams/ProductivityTracker-PersonalProject2.5"
+                >
+                  Deployed
+                </Button>
+              </ButtonGroup>
             </Card.Body>
           </Card>
 
@@ -39,18 +53,20 @@ const Portfolio = () => {
               <Card.Title>Klettersteig</Card.Title>
               <Card.Subtitle>Group Project</Card.Subtitle>
               <Card.Text>App description.</Card.Text>
-              <Button
-                variant="primary"
-                href="https://github.com/Windowmac/Klettersteig"
-              >
-                GitHub
-              </Button>
-              <Button
-                variant="secondary"
-                href="https://sheltered-badlands-95600.herokuapp.com/"
-              >
-                Deployed
-              </Button>
+              <ButtonGroup aria-label="Basic example">
+                <Button
+                  variant="primary"
+                  href="https://github.com/Windowmac/Klettersteig"
+                >
+                  GitHub
+                </Button>
+                <Button
+                  variant="secondary"
+                  href="https://sheltered-badlands-95600.herokuapp.com/"
+                >
+                  Deployed
+                </Button>
+              </ButtonGroup>
             </Card.Body>
           </Card>
 
@@ -60,41 +76,45 @@ const Portfolio = () => {
               <Card.Title>Profile Generator</Card.Title>
               <Card.Subtitle>Solo Project</Card.Subtitle>
               <Card.Text>App description.</Card.Text>
-              <Button
-                variant="primary"
-                href="https://github.com/andycwilliams/ProfileGen-Week10Homework"
-              >
-                GitHub
-              </Button>
-              <Button
-                variant="secondary"
-                href="https://github.com/andycwilliams/ProfileGen-Week10Homework"
-              >
-                Deployed
-              </Button>
+              <ButtonGroup aria-label="Basic example">
+                <Button
+                  variant="primary"
+                  href="https://github.com/andycwilliams/ProfileGen-Week10Homework"
+                >
+                  GitHub
+                </Button>
+                <Button
+                  variant="secondary"
+                  href="https://github.com/andycwilliams/ProfileGen-Week10Homework"
+                >
+                  Deployed
+                </Button>
+              </ButtonGroup>
             </Card.Body>
           </Card>
         </Row>
 
-        <Row>
+        <Row className="justify-content-md-center">
           <Card style={{ width: "18rem" }}>
             <Card.Img variant="top" src="/img/EmployeeTracker.png" />
             <Card.Body>
               <Card.Title>Employee Tracker</Card.Title>
               <Card.Subtitle>Solo Project</Card.Subtitle>
               <Card.Text>App description.</Card.Text>
-              <Button
-                variant="primary"
-                href="https://github.com/andycwilliams/EmployeeTracker-Week12Homework"
-              >
-                GitHub
-              </Button>
-              <Button
-                variant="secondary"
-                href="https://github.com/andycwilliams/EmployeeTracker-Week12Homework"
-              >
-                Deployed
-              </Button>
+              <ButtonGroup aria-label="Basic example">
+                <Button
+                  variant="primary"
+                  href="https://github.com/andycwilliams/EmployeeTracker-Week12Homework"
+                >
+                  GitHub
+                </Button>
+                <Button
+                  variant="secondary"
+                  href="https://github.com/andycwilliams/EmployeeTracker-Week12Homework"
+                >
+                  Deployed
+                </Button>
+              </ButtonGroup>
             </Card.Body>
           </Card>
 
@@ -104,18 +124,20 @@ const Portfolio = () => {
               <Card.Title>Personal Library</Card.Title>
               <Card.Subtitle>Group Project</Card.Subtitle>
               <Card.Text>App description.</Card.Text>
-              <Button
-                variant="primary"
-                href="https://github.com/bleufox/Personal-Library"
-              >
-                GitHub
-              </Button>
-              <Button
-                variant="secondary"
-                href="https://github.com/bleufox/Personal-Library"
-              >
-                Deployed
-              </Button>
+              <ButtonGroup aria-label="Basic example">
+                <Button
+                  variant="primary"
+                  href="https://github.com/bleufox/Personal-Library"
+                >
+                  GitHub
+                </Button>
+                <Button
+                  variant="secondary"
+                  href="https://github.com/bleufox/Personal-Library"
+                >
+                  Deployed
+                </Button>
+              </ButtonGroup>
             </Card.Body>
           </Card>
 
@@ -125,18 +147,20 @@ const Portfolio = () => {
               <Card.Title>Placeholder</Card.Title>
               <Card.Subtitle>Solo Project</Card.Subtitle>
               <Card.Text>App description.</Card.Text>
-              <Button
-                variant="primary"
-                href="https://github.com/bleufox/Personal-Library"
-              >
-                GitHub
-              </Button>
-              <Button
-                variant="secondary"
-                href="https://github.com/bleufox/Personal-Library"
-              >
-                Deployed
-              </Button>
+              <ButtonGroup aria-label="Basic example">
+                <Button
+                  variant="primary"
+                  href="https://github.com/bleufox/Personal-Library"
+                >
+                  GitHub
+                </Button>
+                <Button
+                  variant="secondary"
+                  href="https://github.com/bleufox/Personal-Library"
+                >
+                  Deployed
+                </Button>
+              </ButtonGroup>
             </Card.Body>
           </Card>
         </Row>
