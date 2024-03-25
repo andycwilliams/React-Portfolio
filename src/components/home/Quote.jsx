2d7ba400b7dca82f1df80329e4e571a49ffec0de
Andy <awilliamscomposing@gmail.com>
2024-03-24 21:46:20 -0700

    Running build, troubleshooting deployment issues

--- src/components/home/Quote.jsx
+++ src/components/home/Quote.jsx
@@ -1,51 +1,51 @@
-// Material UI Imports
-import Avatar from "@mui/material/Avatar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Checkbox from "@mui/material/Checkbox";
-import Chip from "@mui/material/Chip";
-import Container from "@mui/material/Container";
-import Divider from "@mui/material/Divider";
-import Fade from "@mui/material/Fade";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Grid from "@mui/material/Grid";
-import Link from "@mui/material/Link";
-import List from "@mui/material/List";
-import ListItem from "@mui/material/ListItem";
-import ListItemText from "@mui/material/ListItemText";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Stack from "@mui/material/Stack";
-import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
-import { useMediaQuery, useTheme } from "@mui/material";
-// Data Imports
-import sageWisdom from "../../../theAnswerToLifeTheUniverseAndEverything/sageWisdom";
+// // Material UI Imports
+// import Avatar from "@mui/material/Avatar";
+// import Box from "@mui/material/Box";
+// import Button from "@mui/material/Button";
+// import Card from "@mui/material/Card";
+// import CardActions from "@mui/material/CardActions";
+// import CardContent from "@mui/material/CardContent";
+// import CardMedia from "@mui/material/CardMedia";
+// import Checkbox from "@mui/material/Checkbox";
+// import Chip from "@mui/material/Chip";
+// import Container from "@mui/material/Container";
+// import Divider from "@mui/material/Divider";
+// import Fade from "@mui/material/Fade";
+// import FormControlLabel from "@mui/material/FormControlLabel";
+// import Grid from "@mui/material/Grid";
+// import Link from "@mui/material/Link";
+// import List from "@mui/material/List";
+// import ListItem from "@mui/material/ListItem";
+// import ListItemText from "@mui/material/ListItemText";
+// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+// import Stack from "@mui/material/Stack";
+// import TextField from "@mui/material/TextField";
+// import Typography from "@mui/material/Typography";
+// import { useMediaQuery, useTheme } from "@mui/material";
+// // Data Imports
+// import sageWisdom from "../../../theAnswerToLifeTheUniverseAndEverything/sageWisdom";
 
-const Quote = () => {
-  const item = sageWisdom[Math.floor(Math.random() * sageWisdom.length)];
+// const Quote = () => {
+//   const item = sageWisdom[Math.floor(Math.random() * sageWisdom.length)];
 
-  return (
-    <Fade in={true} timeout={1000}>
-      <Box sx={{ mx: { xs: "0px", sm: "100px" } }}>
-        <Card sx={{ p: "20px", borderRadius: "25px" }}>
-          <Box sx={{ textAlign: "center" }}>
-            <Typography sx={{ fontSize: 30, fontStyle: "italic" }}>
-              {item.text}
-            </Typography>
-          </Box>
-          <Box sx={{ textAlign: "right", marginTop: 2 }}>
-            <Typography variant="subtitle1" sx={{ fontSize: 20 }}>
-              {item.author}
-            </Typography>
-          </Box>
-        </Card>
-      </Box>
-    </Fade>
-  );
-};
+//   return (
+//     <Fade in={true} timeout={1000}>
+//       <Box sx={{ mx: { xs: "0px", sm: "100px" } }}>
+//         <Card sx={{ p: "20px", borderRadius: "25px" }}>
+//           <Box sx={{ textAlign: "center" }}>
+//             <Typography sx={{ fontSize: 30, fontStyle: "italic" }}>
+//               {item.text}
+//             </Typography>
+//           </Box>
+//           <Box sx={{ textAlign: "right", marginTop: 2 }}>
+//             <Typography variant="subtitle1" sx={{ fontSize: 20 }}>
+//               {item.author}
+//             </Typography>
+//           </Box>
+//         </Card>
+//       </Box>
+//     </Fade>
+//   );
+// };
 
-export default Quote;
+// export default Quote;
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,6 +3,13 @@ import ContactForm from "./ContactForm";
 import Hero from "./Hero";
 import PortfolioBrief from "./PortfolioBrief";
 import ResumeBrief from "./ResumeBrief";
-import Quote from "./Quote";
+// import Quote from "./Quote";
 
-export { About, ContactForm, Hero, PortfolioBrief, ResumeBrief, Quote };
+export {
+  About,
+  ContactForm,
+  Hero,
+  PortfolioBrief,
+  ResumeBrief,
+  // Quote
+};
